ec1d17337192a2082c54d26cfd9a31cbe13f5830
retrychx <863129776@qq.com>
2025-04-21 21:24:32 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -107,7 +107,7 @@ async function generateWeatherSVG() {
     <rect width="${svgWidth}" height="${svgHeight}" fill="transparent"/>
     <text x="${
       svgWidth / 2
-    }" y="20" font-family="Arial" font-size="16" font-weight="bold" text-anchor="middle" fill="white" stroke="black" stroke-width="0.5">连江县七天天气预报</text>
+    }" y="20" font-family="Arial" font-size="16" font-weight="bold" text-anchor="middle" fill="white" stroke="black" stroke-width="0.5">杭州市七天天气预报</text>
 `;
 
     // 并行加载所有图标
